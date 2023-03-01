70da1529d35ab099e7a30fcf33ad4bb84c0e8dc8
Breno Lira <brenoliradev@gmail.com>
2023-02-28 23:01:17 -0300

    implements color guide from curiouscat colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 const config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: 'rgb(222, 124, 0)',
+        gray: '#E3E3E3',
+        dark: '#1D1E22',
+        medium: 'rgb(37,37,43)',
+        light: 'rgb(78, 78, 78)'
+      }
+    }
   },
   plugins: []
 }
