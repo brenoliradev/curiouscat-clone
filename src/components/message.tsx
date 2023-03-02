74f5af7d36ff3a5645bdd6f02ec2df3792dc7bc2
Breno Lira <brenoliradev@gmail.com>
2023-03-02 10:34:52 -0300

    removes hydration workaround since it apparentelly isnt needed

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,18 +1,7 @@
 import { parseText } from '@/utils/parseText'
-import { useEffect, useState } from 'react'
 
 export const Message = ({ message }: { message: string }) => {
   const parsedMessage = parseText(message)
-  const [hydration, setHydration] = useState<boolean>(false)
-
-  // workaround of hydration error
-  // needed that because the parseText is different
-  // from serverside text (initial hydration mismatch)
-  useEffect(() => {
-    setHydration(true)
-  }, [])
-
-  if (!hydration) return <></>
 
   return (
     <span
