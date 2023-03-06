15b3bb4540dd8acb06f049dcdc739598ce416423
Breno Lira <brenoliradev@gmail.com>
2023-03-05 22:05:13 -0300

    align to start to make it sticks in the correct place

--- src/components/adressee.tsx
+++ src/components/adressee.tsx
@@ -30,7 +30,7 @@ export const Adressee = ({
           className="aspect-square rounded-full border-4"
         />
       )}
-      <div>
+      <div className='flex flex-col items-start'>
         <Link
           href={`/${addresseeData.username}`}
           className="flex justify-center gap-0.5 text-sm font-semibold text-gray"
@@ -47,7 +47,7 @@ export const Adressee = ({
             <></>
           )}{' '}
         </Link>
-        <p className="text-base text-light">
+        <p className="text-sm text-light">
           {readableElapsed(seconds_elapsed)}
         </p>
       </div>
